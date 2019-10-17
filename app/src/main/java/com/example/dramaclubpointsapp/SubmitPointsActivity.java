c5c4ed6c3befd04a7c5c4682238883243960cd30
BottomKek <hannon1253@students.d211.org>
2019-10-17 11:57:49 -0500

    thursday 10/17

--- app/src/main/java/com/example/dramaclubpointsapp/SubmitPointsActivity.java
+++ app/src/main/java/com/example/dramaclubpointsapp/SubmitPointsActivity.java
@@ -2,7 +2,9 @@ package com.example.dramaclubpointsapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class SubmitPointsActivity extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class SubmitPointsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_submit_points);
+    }
+
+    public void goHomeButton(View v){
+
+        Intent intent = new Intent(this, SubmitPointsActivity.class);
+        startActivity(intent);
     }
 }
